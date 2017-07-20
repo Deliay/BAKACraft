192dde440ea8e80ddd76c6d706c0298db5155dcf
Deliay <admin@remiliascarlet.com>
2017-07-20 13:12:56 +0800

    Replace maven dependency to jar package reference directly

--- src/main/java/bakacraft/EnemySpawn/Enemy.java
+++ src/main/java/bakacraft/EnemySpawn/Enemy.java
@@ -1,11 +1,21 @@
 package bakacraft.EnemySpawn;
 
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
 /*
 /**
  * Created by admin on 2017/7/16.
  */
-public class EnemyModel {
-    /*
+public interface Enemy {
+
     public int getRequirPlayerLevel();
 
     public String getCustomName();
@@ -39,5 +49,5 @@ public class EnemyModel {
     public double getKillExp();
 
     public boolean isSpawnThisTick();
-    */
+
 }
